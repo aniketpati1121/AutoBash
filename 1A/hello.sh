3daf65589cb8675109de75a29471896772fdc559
Aniket Patil <aniketpatil2027@gmail.com>
2026-02-08 16:36:33 +0530

    add

--- 1A/hello.sh
+++ 1A/hello.sh
@@ -1,1 +1,11 @@
+#!/bin/bash 
+
 #!/bin/bash
+
+# This is script for TWS
+
+echo "TWS: Hello Dosto"
+
+echo "Learners: DevOps Wale bhaiya , hum toh comment karenge"
+
+echo "TWS: Toh dosto, like bhi kar do"
